914718114548f4b7a77f4d2b0d65b4177d20c9e4
Jacob Fahey (1000003139) <JAKEF1@student.op.ac.nz>
2017-06-16 15:45:55 +1200

    preparing for Heroku

--- Exam/flaskr/flaskr/flaskr.py
+++ Exam/flaskr/flaskr/flaskr.py
@@ -26,6 +26,19 @@ def connect_db():
     return rv
 	
 	
+def init_db():
+    db = get_db()
+    with app.open_resource('schema.sql', mode='r') as f:
+        db.cursor().executescript(f.read())
+    db.commit()
+
+@app.cli.command('initdb')
+def initdb_command():
+    """Initializes the database."""
+    init_db()
+    print('Initialized the database.')
+	
+	
 def get_db():
     """Opens a new database connection if there is none yet for the
     current application context.
@@ -40,19 +53,6 @@ def close_db(error):
     """Closes the database again at the end of the request."""
     if hasattr(g, 'sqlite_db'):
         g.sqlite_db.close()
-		
-
-def init_db():
-    db = get_db()
-    with app.open_resource('schema.sql', mode='r') as f:
-        db.cursor().executescript(f.read())
-    db.commit()
-
-@app.cli.command('initdb')
-def initdb_command():
-    """Initializes the database."""
-    init_db()
-    print('Initialized the database.')
 	
 
 @app.route('/')
